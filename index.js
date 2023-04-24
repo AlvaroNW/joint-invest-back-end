4525fe75a34fd28b48ef203721cb66e5868ae6bc
Alvaro Perez Pintado <122454645+AlvaroNW@users.noreply.github.com>
2023-04-24 09:46:06 +0200

    folder structure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Backend for Joint Invest - from the Behive with &#x2764;&#xFE0F;!')
+  res.send('Backend for Joint Invest - from the Beehive with &#x2764;&#xFE0F;!')
 })
 
 app.listen(port, () => {
