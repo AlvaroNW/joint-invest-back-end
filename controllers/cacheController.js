06b3b291c74f7d6e44a9eead4b41cac4d54fd4fe
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-05-07 16:51:30 +0200

    not caching errors

--- controllers/cacheController.js
+++ controllers/cacheController.js
@@ -5,9 +5,12 @@ const middlewareToGetAPIcalls = async (req, res, next) => {
   try {
     const { remoteUrl, cacheKey } = req.body;
     const { data } = await axios.get(remoteUrl);
+    if (data.status == "error") {
+      console.log(data);
+      res.send(data);
+    }
     cryptoCache.set(cacheKey, data);
     console.log("using external api call -->", cacheKey);
-    console.log(data);
     res.send(data);
   } catch (e) {
     next({ message: e.message });
