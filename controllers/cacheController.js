41cf3f25065e8ba99557adcdcc95ff48bc847aa4
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-05-08 09:02:34 +0200

    hot fix

--- controllers/cacheController.js
+++ controllers/cacheController.js
@@ -5,10 +5,6 @@ const middlewareToGetAPIcalls = async (req, res, next) => {
   try {
     const { remoteUrl, cacheKey } = req.body;
     const { data } = await axios.get(remoteUrl);
-    if (data.status == "error") {
-      console.log(data);
-      res.send(data);
-    }
     cryptoCache.set(cacheKey, data);
     console.log("using external api call -->", cacheKey);
     res.send(data);
